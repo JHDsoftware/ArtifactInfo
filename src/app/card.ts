a02f3040030e07cc05b56d9e1333cd74b2c67bfc
juhaodong <juhaodong@gmail.com>
2020-03-31 09:13:18 +0200

    更新

--- src/app/card.ts
+++ src/app/card.ts
@@ -1,13 +1,61 @@
 export class Card {
     id: number;
-    imageURL: string;
-    base_card_id: string;
-    card_type: string;
-    card_name: string;
-    card_text: string;
-    mini_image: string;
-    large_image:{
-        schinese: string;
-    };
 
+    type: string;
+    rarity: string;
+    name: string;
+    text: string;
+    image:{
+        mini: string;
+        large: string;
+        inGame: string;
+        illustrator: string;
+    };
+    color: number;
+    ref: CardRef[];
+
+
+    //if item
+    goldCost: number;
+    subType: string;
+
+    attack: number;
+    hitPoint: number;
+    armor: number;
+
+    manaCost: number;
+
+}
+/*
+export class Item extends Card{
+    //if item
+    goldCost: number;
+    subType: string;
+
+}
+export class Hero extends Card{
+    attack: number;
+    hitPoint: number;
+    armor: number;
+}
+export class Creep extends Card{
+    manaCost: number;
+    attack: number;
+    hitPoint: number;
+}
+export class Ability extends Card{
+
+}
+export class PassiveAbility extends Card{
+
+}
+export class Spell extends Card{
+    manaCost: number;
+}
+*/
+
+
+export class CardRef {
+    card: Card;
+    refType: string
 }
